73a0e901d96570d222084c7d775f183f5154c0ff
MMilton1 <murraylmilton@outlook.com>
2024-11-21 17:15:51 +0100

    added the function example

---
+++ Functions/app.py
@@ -0,0 +1,18 @@
+def greet(name):
+    print("Hello, " + name)
+    print("Lets learn Python Functions")
+
+
+greet("Murray")
+
+
+# Arguments
+def saySomething(age, current_year,  first_name, last_name):
+    date_of_birth = int(current_year - age)
+    print(f"Hello, {first_name } {last_name}! Your year of birth is {date_of_birth}")
+
+
+saySomething(33, 2024, "Murray", "Murray")
+
+# Types of Functions
+
---
+++ Todo/todo_app.py
@@ -0,0 +1,16 @@
+todo_list = []
+
+new_todo_1 = input("Enter your new todo:")
+todo_list.append(new_todo_1)
+new_todo_2 = input("Enter your new todo:")
+todo_list.append(new_todo_2)
+new_todo_3 = input("Enter your new todo:")
+todo_list.append(new_todo_3)
+new_todo_4 = input("Enter your new todo:")
+todo_list.append(new_todo_4)
+new_todo_5 = input("Enter your new todo:")
+todo_list.append(new_todo_5)
+
+
+print(f"The new todo of {todo_list} has been added to your todos")
+print(type(todo_list))
