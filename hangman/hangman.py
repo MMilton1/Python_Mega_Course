5577391d7b8cf323d4ef524960dd28cfc86ad4cf
MMilton1 <murraylmilton@outlook.com>
2024-11-26 09:35:54 +0100

    added the list to account for each char guessed correctly

--- hangman/hangman.py
+++ hangman/hangman.py
@@ -25,23 +25,39 @@ word_list = [
     "treasure",
     "family"]
 
-# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word. Then print it.
-word_picked = random.choice(word_list)
-print(word_picked)
-# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+chosen_word = random.choice(word_list)
+print(chosen_word)
+
 placeholder = ""
-word_length = len(word_picked)
+word_length = len(chosen_word)
 for position in range(word_length):
     placeholder += "_"
 print(placeholder)
 
-user_guest = input("Guess a letter: ").lower()
+# TODO-1: - Use a while loop to let the user guess again.
+game_over = False
+correct_letters = []
 
-display = ""
+while not game_over:
+    guess = input("Guess a letter: ").lower()
 
-for letter in word_picked:
-    if letter == user_guest:
-        display += letter
-    else:
-        display += "_"
-print(display)
+
+    display = ""
+
+# TODO-2: Change the for loop so that you keep the previous correct letters in display.
+
+    for letter in chosen_word:
+        if letter == guess:
+            display += letter
+            correct_letters.append(letter)
+        elif letter in correct_letters:
+            display += letter
+        else:
+            display += "_"
+    print(display)
+    if "_" not in display:
+        game_over = True
+        print("You win.")
+
+
+
